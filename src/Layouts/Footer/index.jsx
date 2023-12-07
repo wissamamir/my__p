31baeb4bcc0776048cb969ae3874530b13ed8219
wissamamir <wiisam131196@gmail.com>
2023-12-07 17:34:52 +0200

    me

---
+++ src/Layouts/Footer/index.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+
+    return (
+        <footer className="footer">
+        <div>
+   <span>2023</span>
+        </div>
+        </footer>
+    )
+}
+
+export default Footer;
